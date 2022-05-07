e2648c40ae4c2d5cc7324e6b28c07c29f6b87e6c
Arron Pirku <99618081+arronpirku@users.noreply.github.com>
2022-05-07 18:04:52 +0200

    Update Drill11.cpp

--- Drill11.cpp
+++ Drill11.cpp
@@ -12,21 +12,27 @@ try {
 
     cout << '|' << setw(10) << "Arron" << '|' << setw(8) << "Pirku"
          << '|' << setw(24) << "arronpirku@gmail.com"
-         << '|' << setw(12) << "+38344597755" << "|\n"
+         << '|' << setw(12) << "+36306537520" << "|\n"
 
          << '|' << setw(10) << "Armond" << '|' << setw(8) << "Pirku"
-         << '|' << setw(24) << "arpirku@hotmail.com"
-         << '|' << setw(12) << "+38344543777" << "|\n"
+         << '|' << setw(24) << "arpirku@gmail.com"
+         << '|' << setw(12) << "+36302594061" << "|\n"
 
          << '|' << setw(10) << "Drenusha" << '|' << setw(8) << "Pirku"
          << '|' << setw(24) << "drpirku@gmail.com"
-         << '|' << setw(12) << "+383444324235" << "|\n"
+         << '|' << setw(12) << "+36305012459" << "|\n"
 
-         << '|' << setw(10)<< "Diedon" << '|' << setw(8) << "Elshani"
-         << '|' << setw(24) << "diedon1@gmail.com"
+         << '|' << setw(10)<< "Art" << '|' << setw(8) << "Sharani"
+         << '|' << setw(24) << "artsharani@gmail.com"
          << '|' << setw(12) << "+36301925494" << "|\n"
 
-    
+         << '|' << setw(10) << "Shkelqim" << '|' << setw(8) << "Hyseni"
+         << '|' << setw(24) << "shkelqah12@gmail.com"
+         << '|' << setw(12) << "+36306537520" << "|\n"
+
+         << '|' << setw(10) << "Amaar" << '|' << setw(8) << "Pirku"
+         << '|' << setw(24) << "amaarpirku@gmail.com"
+         << '|' << setw(12) << "+36304103785" << "|\n";
         int a;
         int b;
         int c;
